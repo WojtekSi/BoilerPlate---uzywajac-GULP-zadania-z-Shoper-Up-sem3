3fcf13a5eb9bf3474d401a59106623a8205c3dd7
wojtek siwek <wojtek.siwek@example.com>
2023-03-13 10:32:10 +0100

    utworzone zadanie i uruchomione - uzywajac paczki imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 
 export default () => (
-	gulp.src('src/images/*')
+	gulp.src('./src/images/*')
 		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
+		.pipe(gulp.dest('./dist/images'))
 );
