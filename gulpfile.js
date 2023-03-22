c7234f9f11f3fd5b68b999691bd087cdafc9f1f0
wojtek siwek <wojtek.siwek@example.com>
2023-03-22 21:29:45 +0100

    nadpisuje wszystko co zrobilem w zadaniu 2 aby pracowac na kodzie z zajec

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,61 @@
-import gulp from 'gulp';
-import imagemin from 'gulp-imagemin';
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const fileInclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
 
-export default () => (
-	gulp.src('./src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./dist/images'))
-);
+const path = {
+    root: "./dist/",
+    cssSrc: "./src/scss/**/*.scss",
+    cssDist: "./dist/css/",
+    imageSrc: "./src/images/**/*",
+    imageDist: "./dist/images/",
+    htmlSrc: "./src/html/index.html"
+};
+
+const css = function() {
+    return gulp.src(path.cssSrc)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.cssDist))
+};
+
+const imageMin = function() {
+    return gulp.src(path.imageSrc)
+        .pipe(imagemin())
+        .pipe(gulp.dest(path.imageDist))
+};
+
+const html = function() {
+    return gulp.src(path.htmlSrc)
+        .pipe(fileInclude({
+            prefix: "@@",
+            basepath: "@file"
+        }))
+        .pipe(gulp.dest(path.root))
+}
+
+const server = function(cb) { 
+    browserSync.init({
+        server: {
+            baseDir: path.root
+        } 
+    });
+
+    cb();
+}
+
+const serverReload = function(cb) { 
+    browserSync.reload();
+    cb();
+}
+
+const watch = function() {
+    gulp.watch(path.cssSrc, gulp.series(css, serverReload));
+    gulp.watch(path.htmlSrc, gulp.series(html, serverReload));
+}
+
+exports.default = gulp.series( html, css, server, watch );
+
+exports.production = gulp.series( imageMin, html, css );
