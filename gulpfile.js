9e21fd70a15d416afee25f202650b4328dad851a
wojtek siwek <wojtek.siwek@example.com>
2023-05-17 21:17:32 +0200

    dodaje gotowy kod z zajec 5 aby na masterze znajdowal sie aktualny kod wspolny z cala grupa

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,18 @@ const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 const fileInclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 const browserSync = require('browser-sync').create();
+const webpack = require('webpack-stream');
+const webpackConfig = require('./webpack.config');
 
 const path = {
     root: "./dist/",
     cssSrc: "./src/scss/**/*.scss",
     cssDist: "./dist/css/",
+    jsSrc: "./src/js/app.js",
+    jsSrcAll: "./src/js/**/*.js",
+    jsDist: "./dist/js/",
     imageSrc: "./src/images/**/*",
     imageDist: "./dist/images/",
     htmlSrc: "./src/html/index.html"
@@ -17,6 +23,13 @@ const path = {
 const css = function() {
     return gulp.src(path.cssSrc)
         .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.cssDist))
+};
+
+const cssProduction = function() {
+    return gulp.src(path.cssSrc)
+        .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest(path.cssDist))
 };
@@ -36,6 +49,12 @@ const html = function() {
         .pipe(gulp.dest(path.root))
 }
 
+const htmlMinify = function() {
+    return gulp.src(path.htmlSrc)
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest(path.root));
+}
+
 const server = function(cb) { 
     browserSync.init({
         server: {
@@ -51,11 +70,19 @@ const serverReload = function(cb) {
     cb();
 }
 
+const javascript = function() {
+    return gulp.src(path.jsSrc)
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest(path.jsDist))
+};
+
 const watch = function() {
     gulp.watch(path.cssSrc, gulp.series(css, serverReload));
+    gulp.watch(path.jsSrcAll, gulp.series(javascript, serverReload));
     gulp.watch(path.htmlSrc, gulp.series(html, serverReload));
 }
 
-exports.default = gulp.series( html, css, server, watch );
+// DODAĆ TASK O NAZWIE NP "CLEAR" KTORY BEDZIE CZYSCIC CALA ZAWARTOSC FOLDERU /DIST, I ODPALAC TO NA POCZATKU TASKU DEFAULT
 
-exports.production = gulp.series( imageMin, html, css );
+exports.default = gulp.series( html, css, javascript, server, watch );
+// exports.production = gulp.series( imageMin, html, htmlMinify, cssProduction );
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,60 @@
-console.log("hello");
+// import { userAge } from "./components/new-component";
+
+// console.log("userAge: ", userAge);
+
+
+// const obj = {
+//     name: "Jan",
+//     surname: "Kowalski",
+//     age: 30
+// };
+
+// const { name: userName, age: userAge } = obj;
+
+// console.log(userName);
+// console.log(userAge);
+
+// const arr = [1, 2, 3];
+
+// const [a, ,b] = arr;
+
+// console.log(a);
+// console.log(b);
+
+// function rest(name, ...items) {
+//     console.log(`Mam na imię ${name} i moje umiejetnosci to: ${items.join()}`);
+// }
+
+// rest('Marcin', 'JS', 'CSS', 'HTML', 'SSSS');
+
+// const spreadArr1 = [1, 2, 3, 4];
+// const spreadArr2 = [5, 6, 7, 8];
+// const spreadArr3 = [...spreadArr1, ...spreadArr2];
+// const string = "Ala ma kota";
+
+// console.log(spreadArr1);
+// console.log(spreadArr2);
+// console.log(spreadArr3);
+// console.log(...string);
+
+
+// const nazwa = name => console.log(`hello ${name}`);
+
+// nazwa('Marcin');
+
+// const element = document.querySelector('h2');
+
+// element.addEventListener('click', () => {
+
+// });
+
+// const name = "jan";
+
+// const obj = {
+//     sayHello: function() {
+
+//     },
+//     sayHello() {
+
+//     }
+// }
